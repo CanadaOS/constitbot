1da829df65ab78f3c8d36d893f38b682e7b89e08
Sushil Tailor <sushil.tailor@gmail.com>
2016-09-24 19:33:23 -0400

    Temporarily added request module. Replace with axios later

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const https = require('https');
+const send = require('./fb/send');
+const msg = require('./fb/assemble');
 
 const app = express();
 app.set('port', process.env.PORT || 5000);
@@ -13,6 +15,39 @@ app.get('/webhook', function(req, res) {
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
     res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  console.log('***webhook data***', JSON.stringify(data));
+
+  /* Ideal State of affairs:
+     let handleMessage = receive(data).then((event) => { return handleMsgPromise(event) });
+     handleMessage().then((stuff) => { send(stuff).to(user)});
+  */
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      pageEntry.messaging.forEach(function(messagingEvent) {
+        const senderID = messagingEvent.sender.id;
+        send(msg.text('i got u fam')).to(senderID);
+      });
+
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
   }
 });
 
